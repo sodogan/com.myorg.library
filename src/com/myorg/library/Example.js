50e4997ebe96a1aa3d130b10570107c149336023
Solen Dogan <solendogan.sap@gmail.com>
2023-07-13 14:48:55 +1200

    initial commit

---
+++ src/com/myorg/library/Example.js
@@ -0,0 +1,68 @@
+/*!
+ * ${copyright}
+ */
+
+// Provides control com.myorg.library.Example.
+sap.ui.define([
+	"./library", 
+	"sap/ui/core/Control", 
+	"./ExampleRenderer"
+], function (library, Control, ExampleRenderer) {
+	"use strict";
+
+	// refer to library types
+	var ExampleColor = library.ExampleColor;
+
+	/**
+	 * Constructor for a new <code>com.myorg.library.Example</code> control.
+	 *
+	 * @param {string} [sId] id for the new control, generated automatically if no id is given
+	 * @param {object} [mSettings] initial settings for the new control
+	 *
+	 * @class
+	 * Some class description goes here.
+	 * @extends sap.ui.core.Control
+	 *
+	 * @author Fiori tools
+	 * @version ${version}
+	 *
+	 * @constructor
+	 * @public
+	 * @alias com.myorg.library.Example
+	 */
+	var Example = Control.extend("com.myorg.library.Example", /** @lends com.myorg.library.Example.prototype */ {
+		metadata: {
+			library: "com.myorg.library",
+			properties: {
+				/**
+				 * The text to display.
+				 */
+				text: {
+					type: "string",
+					group: "Data",
+					defaultValue: null
+				},
+				/**
+				 * The color to use (default to "Default" color).
+				 */
+				color: {
+					type: "com.myorg.library.ExampleColor",
+					group: "Appearance",
+					defaultValue: ExampleColor.Default
+				}
+			},
+			events: {
+				/**
+				 * Event is fired when the user clicks the control.
+				 */
+				press: {}
+			}
+		},
+		renderer: ExampleRenderer,
+    onclick: function() {
+      this.firePress();
+    }
+	});
+	return Example;
+
+});
---
+++ src/com/myorg/library/library.js
@@ -0,0 +1,67 @@
+/*!
+ * ${copyright}
+ */
+
+/**
+ * Initialization Code and shared classes of library com.myorg.library.
+ */
+sap.ui.define([
+	"sap/ui/core/library"
+], function () {
+	"use strict";
+
+	// delegate further initialization of this library to the Core
+	// Hint: sap.ui.getCore() must still be used to support preload with sync bootstrap!
+	sap.ui.getCore().initLibrary({
+		name: "com.myorg.library",
+		version: "${version}",
+		dependencies: [ // keep in sync with the ui5.yaml and .library files
+			"sap.ui.core"
+		],
+		types: [
+			"com.myorg.library.ExampleColor"
+		],
+		interfaces: [],
+		controls: [
+			"com.myorg.library.Example"
+		],
+		elements: [],
+		noLibraryCSS: false // if no CSS is provided, you can disable the library.css load here
+	});
+
+	/**
+	 * Some description about <code>library</code>
+	 *
+	 * @namespace
+	 * @name com.myorg.library
+	 * @author Fiori tools
+	 * @version ${version}
+	 * @public
+	 */
+	var thisLib = com.myorg.library;
+
+	/**
+	 * Semantic Colors of the <code>com.myorg.library.Example</code>.
+	 *
+	 * @enum {string}
+	 * @public
+	 */
+	thisLib.ExampleColor = {
+
+		/**
+		 * Default color (brand color)
+		 * @public
+		 */
+		Default : "Default",
+
+		/**
+		 * Highlight color
+		 * @public
+		 */
+		Highlight : "Highlight"
+
+	};
+
+	return thisLib;
+
+});
